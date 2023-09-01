37dc53537915eabce524eb32bec6c21c8f4e3a6b
xiaoyang0908 <117154989+xiaoyang0908@users.noreply.github.com>
2023-09-01 11:19:11 +0200

    Add files via upload

--- SourceGraphFinder/src/dtu/gruppe6/Runnable.java
+++ SourceGraphFinder/src/dtu/gruppe6/Runnable.java
@@ -2,21 +2,26 @@ package dtu.gruppe6;
 
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
 
 public class Runnable { //Calling main main is discouraged
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws IOException{
 		String workingDirectory = System.getProperty("user.dir");
 		String Path = workingDirectory + "/course-02242-examples-main"; //Starting path
 		ArrayList<File> subfolders = new ArrayList<File>();
 		ArrayList<File> allfolders = new ArrayList<File>();
 		ArrayList<File> files = new ArrayList<File>();
+		ArrayList<FileAtrribute> fileInfo = new ArrayList<>();
 		allfolders.add(new File(Path));
 		subfolders.add(new File(Path));
 
@@ -39,30 +44,84 @@ public class Runnable { //Calling main main is discouraged
 					//Finds all java files
 					if(file.getName().endsWith(".java")) {
 						files.add(file);
+						String keyword = file.getName();
+						keyword = keyword.replace(".java", "");
+						Runnable outer = new Runnable();
+						Runnable.FileAtrribute fa = outer.new FileAtrribute(keyword,file.getPath());
+						fileInfo.add(fa);
 					}
 				}
 			}
 		}
-
 		//Prints all files
 		PrintFileFolder(files);
 		System.out.println("Files found: " + files.size());
-		String data;
-		data = getFileData(files.get(0));
-		//remove commented lines
-		data = data.replaceAll("(?:/\\*(?:[^*]|(?:\\*+[^*/]))*\\*+/)|(?://.*)","")
-				   .replaceAll("(?m)^\\s*$", "");
-		System.out.println(data);
-	}
-	public static String getFileData(File file){
-		try {
-			String content = Files.readString(file.toPath());
-			return content;
-		} catch (IOException e) {
-			e.printStackTrace();
+
+		//prints all the dependencies
+		ArrayList<ArrayList<String>> de = FindDenpendencies(fileInfo);
+		for (ArrayList<String> arrayList : de) {
+			if (arrayList.size()==1) {
+				System.out.println("There is no dependency in "+arrayList.get(0));
+			}else{
+				System.out.print(arrayList.get(0)+"'s dependencies is ");
+				for (int i=1;i< arrayList.size();i++) {
+					if (i<arrayList.size()-1) {
+						System.out.print(arrayList.get(i)+",");
+					}else{
+						System.out.println(arrayList.get(i)+";");
+					}
+					
+				}
+			}
+			
 		}
-		return null;
+
+		
+		// String data;
+		// data = getFileData(files.get(0));
+		// //remove commented lines
+			
+		// System.out.println(data);
+
+
 	}
+	//find all the dependencies in files
+	public static ArrayList<ArrayList<String>> FindDenpendencies(ArrayList<FileAtrribute> fileInfo) throws IOException{
+
+		ArrayList<ArrayList<String>> dependencies = new ArrayList<>();
+		for (FileAtrribute fa: fileInfo) {
+			ArrayList<String> s = new ArrayList<>();
+			s.add(fa.fileName);
+			FileInputStream inputStream = new FileInputStream(fa.filePath);
+			BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+			String line;
+			String keyword,str;
+			while((line = bufferedReader.readLine())!=null){
+				line = line.replaceAll("(?:/\\*(?:[^*]|(?:\\*+[^*/]))*\\*+/)|(?://.*)","")
+				   .replaceAll("(?m)^\\s*$", "");
+				if (line.contains("import")  ) {
+					str = line.substring(0, line.indexOf(" "));
+					keyword = line.substring(str.length()+1, line.length()-1);
+					s.add(keyword);
+				}
+				
+			}
+			dependencies.add(s);
+		}
+		return dependencies;
+		
+	}
+    
+
+	// public static String getFileData(File file){
+	// 	try {
+	// 		String content = Files.readString(file.toPath());
+	// 		return content;
+	// 	} catch (IOException e) {
+	// 		e.printStackTrace();
+	// 	}
+	// 	return null;
+	// }
 
 
 	public static ArrayList<File> findSubFolders(ArrayList<File> subfolders) {
@@ -85,4 +144,28 @@ public class Runnable { //Calling main main is discouraged
 		}
 	}
 
+	
+	public class FileAtrribute{
+		String fileName;
+		String filePath;
+		public FileAtrribute(String fileName, String filePath) {
+			this.fileName = fileName;
+			this.filePath = filePath;
+		}
+		public String getFileName() {
+			return fileName;
+		}
+		public void setFileName(String fileName) {
+			this.fileName = fileName;
+		}
+		public String getFilePath() {
+			return filePath;
+		}
+		public void setFilePath(String filePath) {
+			this.filePath = filePath;
+		}
+		
+	}
 }
+
+
