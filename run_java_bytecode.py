b12e470d6b616774787bd8d4b72ab76faccc7790
Collinnn <51782585+Collinnn@users.noreply.github.com>
2023-09-13 17:13:48 +0200

    Fields update

--- run_java_bytecode.py
+++ run_java_bytecode.py
@@ -5,6 +5,54 @@ directory = os.path.dirname(__file__)
 relative_path = "AI-assignments/Classes/student/dtu"
 full_path = os.path.join(directory, relative_path)
 
+def printGraph(graph):
+    if os.path.exists("graph.dot"):
+        os.remove("graph.dot")
+    # Create a new file
+    f = open("graph.dot", "a")
+
+
+def graphfields(self,fields):
+    return
+
+def fieldType(field):
+    
+    if 'base' in field['type']:
+        return field['type']['base']
+    if 'kind' in field['type']:
+        if(field['type']['kind'] == 'class'):
+            
+            if 'inner' in field['type']:
+                if str(field['type']['inner']) == "None":
+                    return field['type']['name']
+                return field['type']['inner']['name']
+                
+        
+        
+
+def simplefields(fieldsjson):
+
+    fields = []
+    for i in range(len(fieldsjson)):
+
+        if(fieldsjson[i]['access'][0] == 'private'):
+            name = fieldsjson[i]['name']
+            string = "-" + str(name) + "() : " + str(fieldType(fieldsjson[i]))
+            fields.append(string)
+            
+        elif(fieldsjson[i]['access'] == 'public'):
+            name = fieldsjson[i]['name']
+            string = "+" + str(name) + "() : " + str(fieldType(fieldsjson[i]))
+            fields.append(string)
+            
+        elif(fieldsjson[i]['access'] == 'protected'):
+            name = fieldsjson[i]['name']
+            string = "#" + str(name) + "() : " + str(fieldType(fieldsjson[i]))
+            fields.append(string)
+        
+    return fields
+
+
 for file_name in os.listdir(full_path):
     if file_name.endswith('AI.json'):
         file = open(os.path.join(full_path, file_name), 'r')
@@ -45,3 +93,19 @@ for file_name in os.listdir(full_path):
             methods.append(access + name + '()' + ':' + returnType)
         print(methods)
   
+        fieldsjson = data['fields']
+        print(file_name)
+        fields = {}
+        if(len(fieldsjson) == 0):
+            continue
+        fields = simplefields(fieldsjson)
+        print(fields)
+
+    
+
+
+            
+        
+        
+        
+    
