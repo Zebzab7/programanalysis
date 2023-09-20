407af71b165826e7e5c81a5d39236382d8d3d315
Zebzab7 <56087724+Zebzab7@users.noreply.github.com>
2023-09-20 15:16:44 +0200

    Added find_method

--- run_bytecode_interpreter.py
+++ run_bytecode_interpreter.py
@@ -1,35 +1,43 @@
 from pathlib import Path
 import json
 
-class Interpreter:
-    
-    # Memory as a dictionary
-    mem = {}
+# Memory as a dictionary
+mem = {}
 
-    # Locals, operational stack, program counter
-    stack = ([], [], 0)
+# Locals, operational stack, program counter
+stack = ([], [], 0)
 
-    # The program
-    path = Path("bin/course-examples/json")
-    cases = []
-    bytecodes = []
-    classes = {}
-    for f in path.glob("**/Simple.json"):
-        with open(f) as json_file:
-            doc = json.load(json_file)
-            classes[doc["name"]] = doc
-    
-    # The program
-    
-    for cls in classes.values():
-        for method in cls["methods"]:
+# The program
+path = Path("bin/course-examples/json")
+cases = []
+bytecodes = []
+classes = {}
+for f in path.glob("**/Simple.json"):
+    with open(f) as json_file:
+        doc = json.load(json_file)
+        classes[doc["name"]] = doc
 
-            for annotation in method["annotations"]:
-                if annotation["type"] == "dtu/compute/exec/Case":
-                    cases.append(method)
-                    bytecodes.append(method["code"]["bytecode"])
-
+# am is absolute method and is structured such that 
+# am[0] = class name and am[1] = method name
+def find_method(am):
+    ms = classes[am[0]]["methods"]
+    for m in ms:
+        if m["name"] == am[1]:
+            return m
         
-    for method in cases:
-        print(method["name"])
-        print(method["code"]["bytecode"])
+# The program
+for cls in classes.values():
+    for method in cls["methods"]:
+
+        for annotation in method["annotations"]:
+            if annotation["type"] == "dtu/compute/exec/Case":
+                cases.append(method)
+                bytecodes.append(method["code"]["bytecode"])
+
+# for method in cases:
+#     print(method["name"])
+#     print(method["code"]["bytecode"])
+
+# print(classes["dtu/compute/exec/Simple"]["name"])
+
+print(find_method(("dtu/compute/exec/Simple", "noop")))
